35e0f84a2dba22fceea14413718bd6bd9d2c74dc
Mariana Junqueira <69199400+junqueiras@users.noreply.github.com>
2020-11-07 09:45:55 -0300

    adding image

--- app/src/main/java/com/example/todayscocktail/MainActivity.kt
+++ app/src/main/java/com/example/todayscocktail/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.todayscocktail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.todayscocktail.repository.CocktailsListRepository
 import com.example.todayscocktail.viewmodel.CocktailsListViewModel
 import com.example.todayscocktail.viewmodel.CocktailsListViewModelFactory
@@ -17,7 +19,13 @@ class MainActivity : AppCompatActivity() {
         val viewModelFactory = CocktailsListViewModelFactory(CocktailsListRepository())
         val viewModel = ViewModelProvider(this, viewModelFactory).get(CocktailsListViewModel::class.java)
         val list = viewModel.cocktailList
+
+
         list.observe(this, Observer {
+            val imageView= findViewById<ImageView>(R.id.imageView);
+            Glide.with(this)
+                    .load(it[0].thumbUrl)
+                    .into(imageView)
             findViewById<TextView>(R.id.textView).text = it[0].name
         })
     }
